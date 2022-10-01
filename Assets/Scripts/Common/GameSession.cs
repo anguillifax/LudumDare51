68f961ccc1c176c38867ae1fda71d049ca6a96e9
Anguillifax <anguillifax@gmail.com>
2022-09-30 23:09:05 -0700

    Add Boilerplate

--- Assets/Scripts/Common/GameSession.cs
+++ Assets/Scripts/Common/GameSession.cs
@@ -2,18 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameJam
 {
 	public class GameSession : MonoBehaviour
 	{
-		public static GameSessionState Current
+		/// <summary>
+		/// Returns the current Game Session. Returns null if it doesn't exist.
+		/// </summary>
+		public static GameSessionState Current => instance.current;
+
+		private static GameSession instance;
+
+		public Letter[] commonEventPool;
+		public Letter[] bossEventPool;
+
+		public int startingCoins = 12;
+
+		private GameSessionState current;
+
+		private void Awake()
 		{
-			get; set;
+			DontDestroyOnLoad(gameObject);
+			instance = this;
 		}
 
-		// TODO
+		private void Start()
+		{
+			SceneManager.LoadScene("MainMenu");
+		}
 
+		public void CreateNewSession()
+		{
+			current = new GameSessionState
+			{
+				coins = startingCoins,
+				currentLevelNumber = 0,
+				commonEvents = new List<Letter>(commonEventPool),
+				bossEvents = new List<Letter>(bossEventPool)
+			};
+		}
 	}
 }
 
--- Assets/Scripts/Rooms/Letter.cs
+++ Assets/Scripts/Rooms/Letter.cs
@@ -7,8 +7,11 @@ namespace GameJam
 	[CreateAssetMenu(menuName ="LD51/New Letter")]
 	public class Letter : ScriptableObject
 	{
+		[Multiline]
 		public string text;
+
 		public int cost;
+
 		public string associatedScene;
 	}
 }
